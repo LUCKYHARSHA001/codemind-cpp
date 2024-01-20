7ee0515c7b3b167b6f931e9f4a5dd39eb6998552
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2024-01-20 23:45:11 +0530

    #include<iostream> using namespace std; int main() {     int x;     cin>>x;     if(4*x<=1000)     {         cout<<"YES";     }     else     {        cout<<"NO";     } }

---
+++ Tickets.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(4*x<=1000)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+       cout<<"NO"; 
+    }
+}
